dd489ee7e37b982401dd414311fb431f79f0b570
Bekti Cahyo <Bektic.22@gmail.com>
2019-08-15 15:46:43 +0700

    news tab

--- app/src/main/java/com/mvvm_di_koin/ui/HomeFragment.kt
+++ app/src/main/java/com/mvvm_di_koin/ui/HomeFragment.kt
@@ -12,6 +12,8 @@ import com.mvvm_di_koin.R
 import com.mvvm_di_koin.module.adapter.NewsAdapter
 import com.mvvm_di_koin.module.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.progress
+import kotlinx.android.synthetic.main.home_fragment.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -21,26 +23,20 @@ class HomeFragment : Fragment() {
     private  val  viewModel: MainViewModel by viewModel { parametersOf(id) }
     private lateinit var newsAdapter: NewsAdapter
 
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        initAdapter()
-        initViewModel()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+        initViewModel()
     }
 
     private fun initAdapter() {
         newsAdapter = NewsAdapter()
-        rv.apply {
+        rvnews.apply {
             layoutManager = GridLayoutManager(context, 3)
             adapter = newsAdapter
         }
